4050d3737ebb6bfc81cdb01b36e5e04bb86e2614
mlkonerman <mlkonerman@gmail.com>
2019-05-22 11:38:54 -0400

    initial commit

---
+++ src/main/java/com/prs/web/UserController.java
@@ -0,0 +1,113 @@
+package com.prs.web;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.prs.business.User;
+import com.prs.db.UserRepository;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping("/")
+	public JsonResponse getAll() {
+		JsonResponse jr = null;
+		try {
+			jr = JsonResponse.getInstance(userRepository.findAll());
+		} catch (Exception e) {
+			jr = JsonResponse.getInstance(e);
+		}
+		return jr;
+	}
+	
+	@GetMapping("/{id}")
+	public JsonResponse get(@PathVariable int id) {
+		JsonResponse jr = null;
+		try {
+			Optional<User> p = userRepository.findById(id);
+			if (p.isPresent())
+				jr = JsonResponse.getInstance(p);
+			else
+				jr = JsonResponse.getInstance("No user found for id " + id);
+		} catch (Exception e) {
+			jr = JsonResponse.getInstance(e);
+		}
+		return jr;
+	}
+	
+	@PostMapping("/")
+	public JsonResponse add(@RequestBody User u) {
+		JsonResponse jr = null;
+		// NOTE: May need to enhance exception handling if more than one exception type
+		// needs to be caught
+		try {
+			jr = JsonResponse.getInstance(userRepository.save(u));
+		} catch (Exception e) {
+			jr = JsonResponse.getInstance(e);
+		}
+		return jr;
+	}
+	
+	@PostMapping("")
+	public JsonResponse getByUserNameAndPassword(@RequestParam String userName, String password) {
+		JsonResponse jr = null;
+		try {
+			Optional<User> u = userRepository.findByUserNameAndPassword(userName, password);
+			if (u.isPresent())
+				jr = JsonResponse.getInstance(u);
+			else
+				jr = JsonResponse.getInstance("Invalid login. Username and password combination not found.");
+		} catch (Exception e) {
+			jr = JsonResponse.getInstance(e);
+		}
+		return jr;
+	}
+
+	
+	@PutMapping("/")
+	public JsonResponse update(@RequestBody User u) {
+		JsonResponse jr = null;
+		// NOTE: May need to enhance exception handling if more than one exception type
+		// needs to be caught
+		try {
+			if (userRepository.existsById(u.getId())) {
+
+				jr = JsonResponse.getInstance(userRepository.save(u));
+			} else {
+				jr = JsonResponse.getInstance(
+						"User id: " + u.getId() + " does not exist and" + "you are attempting to save it.");
+			}
+		} catch (Exception e) {
+			jr = JsonResponse.getInstance(e);
+		}
+		return jr;
+	}
+
+	@DeleteMapping("/")
+	public JsonResponse delete(@RequestBody User u) {
+		JsonResponse jr = null;
+		// NOTE: May need to enhance exception handling if more than one exception type
+		// needs to be caught
+		try {
+			if (userRepository.existsById(u.getId())) {
+				userRepository.delete(u);
+				jr = JsonResponse.getInstance("User deleted.");
+			} else {
+				jr = JsonResponse.getInstance(
+						"User id: " + u.getId() + " does not exist and" + "you are attempting to delete it.");
+			}
+		} catch (Exception e) {
+			jr = JsonResponse.getInstance(e);
+		}
+		return jr;
+	}
+
+
+
+}
